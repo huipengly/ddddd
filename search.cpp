089e449435ec7982bd0d92e5fd5a87393021f443
huipengly <huipengly@gmail.com>
2018-04-23 01:54:43 +0800

    将数据做成二维数组，将网络拓补图作为矩阵。

--- search.cpp
+++ search.cpp
@@ -1,8 +1,48 @@
 #include "search.h"
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+extern int grid_topo_data[956][3];
+extern int request_data[4001][500];
+int map[500][500] = {0};
+
+vector<request> request_vec[1000];
 
 //你要完成的功能总入口
 void search()
 {
+    make_map();
 
+    for (int  i= 0; i < 500; ++i)
+    {
+        for (int j = 0; j < 500; ++j)
+        {
+            cout << map[i][j] << '\t';
+        }
+        cout << endl;
+    }
+}
+
+void make_map()
+{
+    for (int data_num = 1; data_num < 956; ++data_num)
+    {
+        int row = grid_topo_data[data_num][0];
+        int col = grid_topo_data[data_num][1];
+        map[row][col] = grid_topo_data[data_num][2];
+        map[col][row] = grid_topo_data[data_num][2];
+    }
+}
+
+void make_request_vec()
+{
+//    int counter = 0;    //记录读取行
+//    while (counter < 4001)
+//    {
+//        request_vec[10]
+//        counter = counter + 4;
+//    }
 }
--- search.h
+++ search.h
@@ -1,10 +1,16 @@
 #ifndef __SEARCH_H__
 #define __SEARCH_H__
 
-#include "lib_io.h"
+#include <vector>
+
+struct request
+{
+    int number;                     //需求编号
+    int request_band_width;         //需求带宽
+    std::vector<std::vector<int>> route[3];   //备用线路
+};
 
 void search();
-
-	
+void make_map();
 
 #endif
